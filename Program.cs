ef95568de1d86e3381f46fe6f03bd8e139a85467
MkCs133 <goranmilanovic2001.gm@gmail.com>
2024-03-26 23:59:33 +0100

    User settings profile interface

--- Program.cs
+++ Program.cs
@@ -45,6 +45,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=User}/{action=UserSettings}/{id?}");
 
 app.Run();
